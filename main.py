5746b0e823b53e7293eea65b05ac66e14be50082
jonathanAnguise <jonathan.anguise@protonmail.com>
2023-01-15 11:44:51 +0100

    read url.txt and open it on new tab

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from connect_manager import *
 from dotenv import dotenv_values
+from file_manager import *
 
 if __name__ == '__main__':
     env_var = dotenv_values()
@@ -10,4 +11,10 @@ if __name__ == '__main__':
     linkedin_session = WindowHandler()
     linkedin_session.connect_to_linkedin(env_var.get("EMAIL"),
                                          env_var.get("PASSWORD_LINKEDIN"))
+    my_file_manager = FileManager()
+    my_file_manager.load_url()
+    for url in my_file_manager.url_list:
+        # Open and navigate new tab
+        linkedin_session.open_new_tab_with_url(url)
+        # TODO 2: Connect with the potential candidate
     input()
